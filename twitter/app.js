c319294460d117735198de8d13a656696148d588
liushuyu <liushuyu011@gmail.com>
2023-01-22 18:38:13 -0700

    tree-wide: restructuring ...
    * make the scripts share the same lock file by using a Yarn workspace
    * replaced winston with fancy-log
    * replaced sync-exec with execa
    * add type annotations to some functions

--- twitter/app.js
+++ twitter/app.js
@@ -1,50 +1,60 @@
-require('checkenv').check();
+require("checkenv").check();
+const logger = require("fancy-log");
+const Twitter = require("twitter");
+const fs = require("fs-extra");
 
-const logger = require('winston')
-const Twitter = require('twitter')
-const dateFormat = require('dateformat')
-const fs = require('fs-extra')
-
-const outputDirectory = '../../../site/content/entry'
+const outputDirectory = "../../../site/content/entry";
 
 const client = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url/22648406#22648406
+/** @param {string} str */
 function isURL(str) {
-	var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
-	var url = new RegExp(urlRegex, 'i');
-	return str.length < 2083 && url.test(str);
+  const urlRegex =
+    "^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$";
+  const url = new RegExp(urlRegex, "i");
+  return str.length < 2083 && url.test(str);
 }
 
-const run = async () => {
-	console.info(`Getting user timeline information for ${process.env.TWITTER_SCREEN_NAME}.`)
-	let tweets = await client.get('statuses/user_timeline', {screen_name: process.env.TWITTER_SCREEN_NAME}).then(tweets => {
-		return tweets.slice(0, process.env.TWITTER_TIMELINE_COUNT + 1 || 50 + 1).map((x, index) => {
-			return {
-				id: x.id_str,
-				date: new Date(x.created_at),
-				author: x.user.screen_name,
-				message: x.text
-			}
-		})
-	})
+async function run() {
+  logger.info(
+    `Getting user timeline information for ${process.env.TWITTER_SCREEN_NAME}.`
+  );
+  let tweets = await client
+    .get("statuses/user_timeline", {
+      screen_name: process.env.TWITTER_SCREEN_NAME,
+    })
+    .then((tweets) => {
+      return tweets
+        .slice(0, process.env.TWITTER_TIMELINE_COUNT + 1 || 50 + 1)
+        .map((x, index) => {
+          return {
+            id: x.id_str,
+            date: new Date(x.created_at),
+            author: x.user.screen_name,
+            message: x.text,
+          };
+        });
+    });
 
-	console.info(`Got ${tweets.length} tweets from the Twitter API.`)
-	// Write each tweet as entry content.
-	return tweets.forEach(async (x) => {
-		if (isURL(x.message)) {
-			console.warn(`Skipping frontmatter generation for tweet ${x.id} -- just a URL.`)
-			return
-		}
-		
-		let tweetRef = `tweet_${x.id}`
-		let frontmatterPath = `${outputDirectory}/${tweetRef}/index.md`
-		let frontmatterContents = `+++
+  logger.info(`Got ${tweets.length} tweets from the Twitter API.`);
+  // Write each tweet as entry content.
+  return tweets.forEach(async (x) => {
+    if (isURL(x.message)) {
+      logger.warn(
+        `Skipping frontmatter generation for tweet ${x.id} -- just a URL.`
+      );
+      return;
+    }
+
+    let tweetRef = `tweet_${x.id}`;
+    let frontmatterPath = `${outputDirectory}/${tweetRef}/index.md`;
+    let frontmatterContents = `+++
 date = "${x.date.toISOString()}"
 title = "Tweet ${x.id} by ${x.author}"
 twitter = true
@@ -52,13 +62,13 @@ twitterId = ${x.id}
 twitterUrl = "https://twitter.com/${x.author}/status/${x.id}"
 +++
 
-${x.message}`
-		await fs.outputFile(frontmatterPath, frontmatterContents)
-		logger.info(`Wrote '${frontmatterPath}' to filesystem.`)
-	})
+${x.message}`;
+    await fs.outputFile(frontmatterPath, frontmatterContents);
+    logger.info(`Wrote '${frontmatterPath}' to filesystem.`);
+  });
 }
 
-run().catch(err => {
-	logger.error(err);
-	process.exit(1);
+run().catch((err) => {
+  logger.error(err);
+  process.exit(1);
 });
--- wiki/app.js
+++ wiki/app.js
@@ -1,111 +1,133 @@
-require('checkenv').check();
+require("checkenv").check();
 
-const util = require('util');
-const logger = require('winston');
+const util = require("util");
+const logger = require("fancy-log");
 
-const sanitizeHtml = require('sanitize-html');
-const fs = require('fs-extra');
-const exec = require('sync-exec');
+const sanitizeHtml = require("sanitize-html");
+const fs = require("fs-extra");
+const execa = import("execa");
 
-const inputDirectory = './wiki/';
-const outputDirectory = '../../../site/content/wiki/';
+const inputDirectory = "./wiki/";
+const outputDirectory = "../../../site/content/wiki/";
 
-// The URL
+/** Generate URL for the given title name
+ * @param {string} title
+ */
 function url(title) {
-	return '/wiki/' + title.replace(/\s+/g, '-').toLowerCase();
+  return "/wiki/" + title.replace(/\s+/g, "-").toLowerCase();
 }
 
-if (fs.existsSync(inputDirectory)) {
-	logger.info(`Purging input directory: ${inputDirectory}`);
-	fs.removeSync(inputDirectory);
+async function run() {
+  if (fs.existsSync(inputDirectory)) {
+    logger.info(`Purging input directory: ${inputDirectory}`);
+    fs.removeSync(inputDirectory);
+  }
+
+  const exec = (await execa).execaSync;
+  logger.info(`git clone ${process.env.GITHUB_WIKI_URL} wiki`);
+  exec("git", ["clone", process.env.GITHUB_WIKI_URL, "wiki"]);
+
+  if (!fs.existsSync(outputDirectory)) {
+    logger.info(`Creating missing output directory: ${outputDirectory}`);
+    fs.mkdirSync(outputDirectory);
+  }
+
+  fs.readdir(inputDirectory, (err, items) => {
+    if (err) {
+      logger.error(err);
+      return;
+    }
+
+    try {
+      // Look for all .md files within the wiki directory.
+      items
+        .filter((file) => file.endsWith(".md"))
+        .forEach((item) => {
+          // Generate the title from the filename.
+          generateEntry(item);
+        });
+    } catch (err) {
+      logger.error(err);
+    }
+  });
 }
 
-const gitCloneCommand = `git clone ${process.env.GITHUB_WIKI_URL} wiki`;
-logger.info(`exec ${gitCloneCommand}`);
-exec(gitCloneCommand);
+/** Generate one wiki entry
+ * @param {string} item */
+function generateEntry(item) {
+  const title = item.replace(/-/g, " ").slice(0, -3);
+  const stats = fs.statSync(`${inputDirectory}${item}`);
+  const modified = new Date(util.inspect(stats.mtime));
 
-if (fs.existsSync(outputDirectory) === false) {
-	logger.info(`Creating missing output directory: ${outputDirectory}`);
-	fs.mkdirSync(outputDirectory);
+  // Read the .md file.
+  fs.readFile(`${inputDirectory}${item}`, "utf8", (err, data) => {
+    if (err) {
+      logger.error(err);
+      return;
+    }
+
+    try {
+      // Convert various data inside of the markdown language.
+      let cleanData = sanitizeHtml(data);
+
+      // Blackfriday Markdown Rendering requires a blank line before lists.
+      try {
+        const lines = cleanData.split(/\r?\n/);
+        for (let i = 0; i < lines.length; i++) {
+          // If it's the start of the file, ignore to prevent an index issue.
+          if (i > lines.length) return;
+          if (i === 0 || lines[i] === "\n") continue;
+
+          // Search for the start of a list designated by the * character.
+          if (
+            lines[i].startsWith("* ") &&
+            lines[i - 1].startsWith("* ") === false
+          ) {
+            i++;
+            lines.splice(i - 1, 0, "");
+          }
+        }
+        cleanData = lines.join("\n");
+      } catch (err) {
+        logger.error(err);
+      }
+
+      // Replacing tags like [[Common Issues on Windows|Common Issues]]
+      cleanData = cleanData.replace(
+        /\[\[(.*)\|(.*)\]\]/g,
+        (_, p1, p2) => `[${p1}](${url(p2)})`
+      );
+
+      // Replacing tags like [[Common Issues]]
+      cleanData = cleanData.replace(
+        /\[\[(.*)\]\]/g,
+        (_, p1) => `[${p1}](${url(p1)})`
+      );
+
+      // Restore some escaped entities
+      cleanData = cleanData
+        .replace(/&amp;/g, "&")
+        .replace(/&lt;/g, "<")
+        .replace(/&gt;/g, ">");
+
+      // Create the new markdown header for Hugo.
+      const newFileContents = `+++\r\ntitle = "${title}"\r\ndate = "${modified.toISOString()}"\r\n+++\r\n\r\n${cleanData}\r\n`;
+
+      const itemOutput = item.toLowerCase();
+      fs.writeFile(
+        `${outputDirectory}${itemOutput}`,
+        newFileContents,
+        (err) => {
+          if (err) {
+            return logger.error(err);
+          }
+          logger.info(`Wrote file ${itemOutput} to filesystem.`);
+        }
+      );
+    } catch (err) {
+      logger.error(err);
+    }
+  });
 }
 
-fs.readdir(inputDirectory, (err, items) => {
-	if (err) {
-		logger.error(err);
-		return;
-	}
-
-	try {
-		// Look for all .md files within the wiki directory.
-		items.filter(file => file.substr(-3) === '.md').forEach(item => {
-			// Generate the title from the filename.
-			const title = item.replace(/-/g, ' ').slice(0, -3);
-			const stats = fs.statSync(`${inputDirectory}${item}`);
-			const modified = new Date(util.inspect(stats.mtime));
-
-			// Read the .md file.
-			fs.readFile(`${inputDirectory}${item}`, 'utf8', (err, data) => {
-				if (err) {
-					logger.error(err);
-					return;
-				}
-
-				try {
-					// Convert various data inside of the markdown language.
-					let cleanData = sanitizeHtml(data);
-
-					// Blackfriday Markdown Rendering requires a blank line before lists.
-					try {
-						const lines = cleanData.split(/\r?\n/);
-						for (let i = 0; i < lines.length; i++) {
-							// If it's the start of the file, ignore to prevent an index issue.
-							if (i > lines.length) {
-								return;
-							}
-							if (i === 0 || lines[i] === '\n') {
-								continue;
-							}
-
-							// Search for the start of a list designated by the * character.
-							if (lines[i].startsWith('* ') && lines[i - 1].startsWith('* ') === false) {
-								i += 1;
-								lines.splice(i - 1, 0, '');
-							}
-						}
-						cleanData = lines.join('\n');
-					} catch (err) {
-						logger.error(err);
-					}
-
-					// Replacing tags like [[Common Issues on Windows|Common Issues]]
-					cleanData = cleanData.replace(/\[\[(.*)\|(.*)\]\]/g, (match, p1, p2) => {
-						return `[${p1}](${url(p2)})`;
-					});
-
-					// Replacing tags like [[Common Issues]]
-					cleanData = cleanData.replace(/\[\[(.*)\]\]/g, (match, p1) => {
-						return `[${p1}](${url(p1)})`;
-					});
-					
-					// Replace decoded &amp; -> &
-					cleanData = cleanData.replace(/&amp;/g, "&");
-
-					// Create the new markdown header for Hugo.
-					const newFileContents = `+++\r\ntitle = "${title}"\r\ndate = "${modified.toISOString()}"\r\n+++\r\n\r\n${cleanData}\r\n`;
-
-					const itemOutput = item.toLowerCase();
-					fs.writeFile(`${outputDirectory}${itemOutput}`, newFileContents, err => {
-						if (err) {
-							return logger.error(err);
-						}
-						logger.info(`Wrote file ${itemOutput} to filesystem.`);
-					});
-				} catch (err) {
-					logger.error(err);
-				}
-			});
-		});
-	} catch (err) {
-		logger.error(err);
-	}
-});
+run().catch((e) => logger.error(e));
